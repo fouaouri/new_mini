419a694ce81bb65b37ac3cc76c55054076a596d3
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2023-08-24 22:25:39 +0100

    Fix: protect checking for no cmds

--- execution/open_pipes.c
+++ execution/open_pipes.c
@@ -6,45 +6,11 @@
 /*   By: melhadou <melhadou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 12:37:50 by melhadou          #+#    #+#             */
-/*   Updated: 2023/08/24 21:58:42 by melhadou         ###   ########.fr       */
+/*   Updated: 2023/08/24 22:24:59 by melhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
-
-/* 
- * open pipes ?? => why i need them
- * fork then open pipe . to begin execution
- * check command and exec
- * [] 
- */
-
-// void	exec_one_cmd(t_list *node, char **env)
-// {
-// 	char	*cmd_full_path;
-// 	int	pid;
-
-// 	cmd_full_path = check_cmd(parse_path(getenv("PATH")), node->commandes[0]);
-// 	if (cmd_full_path)
-// 	{
-// 		pid = fork();
-// 		// causes a segfualt
-// 		if (pid == 0)
-// 		{
-// 			if (node->outfile != STDOUT_FILENO)
-// 			{
-// 				dup2(node->outfile, STDOUT_FILENO);
-// 			}
-// 			if (node->infile != STDIN_FILENO)
-// 			{
-// 				dup2(node->infile, STDIN_FILENO);
-// 			}
-// 			if (execve(cmd_full_path, node->commandes, env) == -1)
-// 				exit(1);
-// 		}
-// 		wait(&pid);
-// 	}
-// }
 
 void	exec_cmd(t_list *node, char **env) {
 	char *cmd_full_path;
@@ -53,7 +19,9 @@ void	exec_cmd(t_list *node, char **env) {
 	int in_fd = node->infile;
 	int out_fd = node->outfile;
 
-	cmd_full_path = check_cmd(parse_path(getenv("PATH")), node->commandes[0]);
+	// FIX: just fix for now
+	if (node->commandes[0])
+		cmd_full_path = check_cmd(parse_path(getenv("PATH")), node->commandes[0]);
 
 	if (cmd_full_path) {
 		pid = fork();
