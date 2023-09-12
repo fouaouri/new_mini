bb6773b867ac5705d0d1a18a965363c68cf3127c
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2023-09-12 19:30:09 +0100

    Fix: exit, args

--- builtins/ft_exit.c
+++ builtins/ft_exit.c
@@ -6,18 +6,82 @@
 /*   By: melhadou <melhadou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 22:29:15 by melhadou          #+#    #+#             */
-/*   Updated: 2023/09/12 17:40:55 by melhadou         ###   ########.fr       */
+/*   Updated: 2023/09/12 19:29:26 by melhadou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
-#include <limits.h>
 
-void ft_exit(char **args)
+static long	nlen(long n)
 {
-	unsigned char exit_code;
-	int result;
-	int i;
+	long	i;
+
+	i = 0;
+	if (n < 0)
+		i++;
+	while (n)
+	{
+		n = n / 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_ltoa(long n)
+{
+	long	len;
+	char	*ret;
+
+	len = 0;
+	if (!n)
+		return (ft_strdup("0"));
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	len = nlen(n);
+	ret = my_malloc(len + 1);
+	if (!ret)
+		return (NULL);
+	*(ret + len--) = '\0';
+	if (n < 0)
+	{
+		*(ret) = '-';
+		n = n * -1;
+	}
+	while (n)
+	{
+		*(ret + len--) = n % 10 + '0';
+		n = n / 10;
+	}
+	return (ret);
+}
+
+static long ft_atol(const char *nptr)
+{
+	unsigned long long	n;
+	int					i;
+	int					sign;
+
+	n = 0;
+	i = 0;
+	sign = 1;
+	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+		sign = 1 - 2 * (nptr[i++] == '-');
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		n = n * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (n * sign);
+}
+
+void	ft_exit(char **args)
+{
+	unsigned char	exit_code;
+	// long res;
+	char *str;
+	int	i;
 
 	i = 0;
 	if (!args[1])
@@ -47,19 +111,13 @@ void ft_exit(char **args)
 		}
 		i++;
 	}
-	// if ((ft_atoi(args[1]) * -1) > INT_MAX)
-	// {
-	// 	ft_dprintf(2, "Minishell: %s: numeric argument required\n", args[1]);
-	// 	exit(2);
-	// }
-	// else
-	result = ft_atoi(args[1]);
-	if (result < INT_MIN)
+	str = ft_ltoa(ft_atol(args[1]));
+	if (ft_strcmp(str, args[1]))
 	{
 		ft_dprintf(2, "Minishell: %s: numeric argument required\n", args[1]);
 		exit(2);
 	}
 	else
-		exit_code = result;
+		exit_code = ft_atol(args[1]);
 	exit(exit_code);
 }
